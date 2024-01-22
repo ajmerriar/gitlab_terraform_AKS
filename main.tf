e3d16d2e440605a3d1440c77205cd293a3bed178
ajmerriar <ajmerriar01@outlook.com>
2024-01-22 22:41:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_container_group" "containergroup" {
   name                = var.container_group_name
   resource_group_name = azurerm_resource_group.aci_rg.name
   location            = azurerm_resource_group.aci_rg.location
-  ip_address_type     = "public"
+  ip_address_type     = "Public"
   dns_name_label      = var.dns_name_label
   os_type             = "Linux"
 
