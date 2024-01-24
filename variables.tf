4103111cfd1478bf93d636e83490279b6755660f
ajmerriar <ajmerriar01@outlook.com>
2024-01-24 22:28:14 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,54 +1,17 @@
-variable "image_registry_server" {
-  description = "The server address of the image registry"
-}
-
-variable "image_registry_username" {
-  description = "The username for the image registry"
-}
-
-variable "image_registry_password" {
-  description = "The password for the image registry"
-}
-variable "resource_group_name" {
-  default     = "aci"
-  description = "The name of the resource group"
-}
-
-variable "location" {
+variable "resource_group_location" {
+  type        = string
   default     = "Canada Central"
-  description = "Azure location"
+  description = "Location of the resource group."
 }
 
-variable "container_group_name" {
-  default     = "myacis"
-  description = "The name of the container group"
+variable "node_count" {
+  type        = number
+  description = "The initial quantity of nodes for the node pool."
+  default     = 2
 }
 
-variable "dns_name_label" {
-  description = "The DNS label/name for the container groups IP"
-  default     = "aciproject"
-}
-
-variable "container_name" {
-  default     = "first"
-  description = "The name of the container"
-}
-
-variable "image_name" {
-  description = "The container image name"
-}
-
-variable "cpu_core_number" {
-  default     = "0.5"
-  description = "The required number of CPU cores of the containers"
-}
-
-variable "memory_size" {
-  default     = "1.5"
-  description = "The required memory of the containers in GB"
-}
-
-variable "port_number" {
-  default     = "3000"
-  description = "A public port for the container"
+variable "username" {
+  type        = string
+  description = "The admin username for the new cluster."
+  default     = "azureadmin"
 }
