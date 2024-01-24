e2e25b6b3a36edc0451be9016612a656cb862b3d
ajmerriar <ajmerriar01@outlook.com>
2024-01-24 22:26:44 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,42 @@
-output "containergroup_id" {
-  description = "The ID of the created container group"
-  value       = "${azurerm_container_group.containergroup.id}"
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-output "containergroup_ip_address" {
-  description = "The IP address of the created container group"
-  value       = "${azurerm_container_group.containergroup.ip_address}"
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
 }
 
-output "containergroup_fqdn" {
-  description = "The FQDN of the created container group"
-  value       = "${azurerm_container_group.containergroup.fqdn}"
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
+
+output "cluster_password" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
+  sensitive = true
+}
+
+output "cluster_username" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
 }
