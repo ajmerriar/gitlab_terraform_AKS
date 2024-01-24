39dc3617940853025157e67669891206e0d6fa4b
ajmerriar <ajmerriar01@outlook.com>
2024-01-24 22:45:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
-  name                = "practice"
+  name                = "weather"
   resource_group_name = azurerm_resource_group.rg.name
 
   dns_prefix = "first-practice-aks"
